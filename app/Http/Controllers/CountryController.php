4541129e1dd5e014b111815223bfceb18f405457
Mohammed Alswailem <mo3sw95@gmail.com>
2020-06-11 13:37:55 +0300

    Added 'Global' to the AJAX response

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -10,6 +10,37 @@ use Illuminate\Http\Request;
 class CountryController extends Controller
 {
     public function changeCountryAPI($name){
+        if($name == 'Global'){
+            $countries = Country::all()->sortBy('name');
+            $total_cases = 0;
+            $new_cases = 0;
+            $total_recovered = 0;
+            $new_recovered = 0;
+            $total_deaths = 0;
+            $new_deaths = 0;
+            foreach($countries as $country){
+                $data = $country->covidData()->orderBy('date', 'desc')->take(2)->get();
+                if(count($data)){
+                    $total_cases += $data[0]->total_confirmed;
+                    $new_cases += $data[0]->total_confirmed - $data[1]->total_confirmed;
+                    $total_recovered += $data[0]->total_recovered;
+                    $new_recovered += $data[0]->total_recovered - $data[1]->total_recovered;
+                    $total_deaths += $data[0]->total_deaths;
+                    $new_deaths += $data[0]->total_deaths - $data[1]->total_deaths;
+                }
+            }
+            $jsonObj[] = [
+                'total_cases' => $total_cases,
+                'new_cases' => $new_cases,
+                'total_recovered' => $total_recovered,
+                'new_recovered' => $new_recovered,
+                'total_deaths' => $total_deaths,
+                'new_deaths' => $new_deaths,
+                'active_cases' => $total_cases - $total_recovered - $total_deaths,
+                'new_active_cases' => $new_cases - $new_recovered - $new_deaths
+            ];
+            return json_encode($jsonObj);
+        }
         $country = Country::where('name', $name)->first();
         if($country != []){
             $data = $country->covidData()->orderBy('date', 'desc')->take(2)->get();
